ac0b38a58f070f531404547ac8d533af0503baae
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2024-05-11 05:51:13 +0700

    .

--- views/Beranda.php
+++ views/Beranda.php
@@ -12,14 +12,13 @@ $Page->showMessage();
     <!-- small box -->
     <div class="small-box bg-info">
         <div class="inner">
-            <h3>150</h3>
-
-            <p>New Orders</p>
+            <h3>EXPORT</h3>
+            <p>&nbsp;</p>
         </div>
         <div class="icon">
             <i class="ion ion-bag"></i>
         </div>
-        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        <a href="#" class="small-box-footer">Klik di sini <i class="fas fa-arrow-circle-right"></i></a>
     </div>
 </div>
 
@@ -27,14 +26,13 @@ $Page->showMessage();
     <!-- small box -->
     <div class="small-box bg-success">
         <div class="inner">
-            <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-            <p>Bounce Rate</p>
+            <h3>IMPORT</h3>
+            <p>&nbsp;</p>
         </div>
         <div class="icon">
             <i class="ion ion-stats-bars"></i>
         </div>
-        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        <a href="#" class="small-box-footer">Klik di sini <i class="fas fa-arrow-circle-right"></i></a>
     </div>
 </div>
 <?= GetDebugMessage() ?>
