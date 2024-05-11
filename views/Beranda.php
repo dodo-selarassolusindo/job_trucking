d403ea7a990e01f44af2cdd1aa483d5a3eda6efc
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2024-05-11 22:28:21 +0700

    .

--- views/Beranda.php
+++ views/Beranda.php
@@ -8,6 +8,34 @@ $Beranda = &$Page;
 <?php
 $Page->showMessage();
 ?>
+<?php
+$q = 'select * from job2';
+$r = ExecuteRows($q);
+?>
+
+<?php $rec_num = 0 ?>
+
+<?php foreach($r as $row) { ?>
+    
+    <div class="small-box <?= $rec_num % 2 == 0 ? 'bg-info' : 'bg-success' ?>">
+        <div class="inner">
+            <h3 class="text-center"><?= $row['Nama'] ?></h3>
+            <p>&nbsp;</p>
+        </div>
+        <div class="icon">
+            <i class="ion ion-bag"></i>
+        </div>
+        <a href="size2" class="small-box-footer">Lanjut <i class="fas fa-arrow-circle-right"></i></a>
+    </div>
+    <?php $rec_num++ ?>
+
+<?php } ?>
+
+<!--
+<div class="small-box bg-info">
+    <a href="beranda" class="small-box-footer"><i class="fas fa-arrow-circle-left"></i> Kembali</a>
+</div>
+
 <div class="small-box bg-info">
     <div class="inner">
         <h3 class="text-center">EXPORT</h3>
@@ -29,4 +57,5 @@ $Page->showMessage();
     </div>
     <a href="size3" class="small-box-footer">Lanjut <i class="fas fa-arrow-circle-right"></i></a>
 </div>
+--!>
 <?= GetDebugMessage() ?>
