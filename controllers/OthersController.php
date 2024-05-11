f273e3816023adc0a97a96eb982286c2195c6053
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2024-05-11 11:33:21 +0700

    Update OthersController.php

--- controllers/OthersController.php
+++ controllers/OthersController.php
@@ -82,7 +82,7 @@ class OthersController extends ControllerBase
         global $Security, $USER_LEVEL_TABLES;
         $url = "";
         foreach ($USER_LEVEL_TABLES as $t) {
-            if ($t[0] == "tanpaunderscore.php") { // Check default table
+            if ($t[0] == "beranda.php") { // Check default table
                 if ($Security->allowList($t[4] . $t[0])) {
                     $url = $t[5];
                     break;
