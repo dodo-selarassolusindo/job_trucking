b056af5f492ee4afb5d5354997fba4ef895acd63
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2024-05-11 19:52:06 +0700

    .

--- views/Size3.php
+++ views/Size3.php
@@ -13,19 +13,21 @@ $q = 'select * from size';
 $r = ExecuteRows($q);
 ?>
 
+<?php $rec_num = 0 ?>
+
 <?php foreach($r as $row) { ?>
-
-<div class="small-box bg-warning">
-    <div class="inner">
-        <h3><?= $row['Ukuran'] ?></h3>
-        <p>&nbsp;</p>
+    <?php // $warna = $rec_num % 2 == 0 ? 'bg-warning' : 'bg-danger' ?>
+    <div class="small-box <?= $rec_num % 2 == 0 ? 'bg-warning' : 'bg-danger' ?>">
+        <div class="inner">
+            <h3><?= $row['Ukuran'] ?></h3>
+            <p>&nbsp;</p>
+        </div>
+        <div class="icon">
+            <i class="ion ion-bag"></i>
+        </div>
+        <a href="size2" class="small-box-footer">Lanjut <i class="fas fa-arrow-circle-right"></i></a>
     </div>
-    <div class="icon">
-        <i class="ion ion-bag"></i>
-    </div>
-    <a href="size2" class="small-box-footer">Lanjut <i class="fas fa-arrow-circle-right"></i></a>
-</div>
-
+    <?php $rec_num++ ?>
 <?php } ?>
 
 <div class="small-box bg-info">
