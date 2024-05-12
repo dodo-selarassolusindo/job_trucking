15ca1f521d0b4d7ecab0743d9c2ffd30d4b3c1df
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2024-05-12 09:35:15 +0700

    .

--- src/userfn.php
+++ src/userfn.php
@@ -65,9 +65,9 @@ function Language_Load()
     //$this->setPhrase("MyID", "MyValue"); // Refer to language file for the actual phrase id
     //$this->setPhraseClass("MyID", "fa-solid fa-xxx ew-icon"); // Refer to https://fontawesome.com/icons?d=gallery&m=free [^] for icon name
     $this->setTablePhrase('size3', 'TblCaption', 'Job ' . $_GET['job']);
-    $size = ExecuteScalar(
-      'select Ukuran from size where SizeID = '.$_GET['size'].''
-    );
+    // $size = ExecuteScalar(
+    //   'select Ukuran from size where SizeID = '.$_GET['size'].''
+    // );
     $this->setTablePhrase(
         'type2',
         'TblCaption',
