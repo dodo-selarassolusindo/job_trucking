a894357a38270a5f8afb3a2f4291fe6185b123d9
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2024-05-12 12:59:20 +0700

    .

--- src/userfn.php
+++ src/userfn.php
@@ -66,10 +66,10 @@ function Language_Load()
     //$this->setPhraseClass("MyID", "fa-solid fa-xxx ew-icon"); // Refer to https://fontawesome.com/icons?d=gallery&m=free [^] for icon name
     $this->setTablePhrase('size3', 'TblCaption', 'Job List :: ' . $_GET['job']);
     // $ukuran = ExecuteScalar('select Ukuran from size where SizeID = '.$_GET['size']);
-    $ukuran = ExecuteScalar('select Ukuran from size where SizeID = 1'); //where SizeID = '.$_GET['size']);
+    $ukuran = ExecuteScalar('select Ukuran from size where SizeID = 1');
     $this->setTablePhrase(
-        'type2',
-        'TblCaption',
+        'type2', 
+        'TblCaption', 
         'Job '
         . $_GET['job']
         . ' - Size '
