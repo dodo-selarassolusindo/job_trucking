56b40fcca863b566a9c4e46a0fa7e15169554f7c
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2024-05-12 12:33:11 +0700

    .

--- src/userfn.php
+++ src/userfn.php
@@ -66,8 +66,8 @@ function Language_Load()
     //$this->setPhraseClass("MyID", "fa-solid fa-xxx ew-icon"); // Refer to https://fontawesome.com/icons?d=gallery&m=free [^] for icon name
     $this->setTablePhrase('size3', 'TblCaption', 'Job List :: ' . $_GET['job']);
     $this->setTablePhrase(
-        'type2', 
-        'TblCaption', 
+        'type2',
+        'TblCaption',
         'Job '
         . $_GET['job']
         . ' - Size '
@@ -185,7 +185,8 @@ function Container_Build($builder)
 
 function getUkuran()
 {
-    $ukuran = ExecuteScalar('select Ukuran from size where SizeID = '.$_GET['size'].'');
+    // $ukuran = ExecuteScalar('select Ukuran from size where SizeID = '.$_GET['size'].'');
+    $ukuran = '40"';
     return $ukuran;
 }
 
