7aa1796141ef5d130878d119ea0550fe65128e3d
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2024-05-20 11:11:40 +0700

    Update Joborder2.php

--- views/Joborder2.php
+++ views/Joborder2.php
@@ -8,8 +8,43 @@ $Joborder2 = &$Page;
 <?php
 $Page->showMessage();
 ?>
-<?php echo 'Hello - this detail page' ?>
+<?php
+$q = '
+    select
+        jo.*
+    from
+        job_order jo
+    where
+        Job2ID = ' . $_GET['job2id'] . '
+        SizeID = ' . $_GET['sizeid'] . '
+        TypeID = ' . $_GET['typeid'] . '
+    ';
+$r = ExecuteRows($q);
+?>
 
+<table>
+    <thead>
+        <tr>
+            <th>Tanggal</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach($r as $row)
+        {
+        ?>
+        <tr>
+            <td><?= $row['Tanggal'] ?></td>
+        </tr>
+        <?php
+        }
+        ?>
+    <tbody>
+</table>
+
+<?php // echo 'Hello - this detail page' ?>
+
+<!--
 this detail is ::
 <br>
 <?= $_GET['job2id'] ?>
@@ -18,4 +53,7 @@ this detail is ::
 <br>
 <?= $_GET['typeid'] ?>
 <br>
+-->
+
+
 <?= GetDebugMessage() ?>
